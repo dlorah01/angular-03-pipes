0ebac1df4585b44025f5b0859a9282515d0a542f
Harold García <dlorahnailuj@hotmail.com>
2020-06-01 12:14:48 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = "Harold";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+  	nombre: "Logan",
+  	clave: "Wolverine",
+  	edad: 500,
+  	direccion: {
+  		calle: "Primera",
+  		casa: "19"
+  	}
+  };
+
+  valorDePromesa = new Promise ( (resolve, reject) => {
+  	setTimeout( () => resolve("Llegó la data!"), 3500);
+  });
+
+  fecha = new Date();
+
+  nombre2 = "harOld jULian gArcIa cHAParro";
+
+  video = "yyYALsXqOtM";
+
+  activar = true;
 }
